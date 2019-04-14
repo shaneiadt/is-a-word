43a2d7f5bd414a06d7ebf5d8daf2fec498a08fc2
Shane O'Moore <Shane.OMoore@livetiles.nyc>
2019-04-14 17:30:41 +0100

    Add some additional unit tests

--- test/getDefinition.test.js
+++ test/getDefinition.test.js
@@ -7,7 +7,7 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 const should = chai.should;
 
-describe('Is A Word Test Suite Package', () => {
+describe('Get Definition', () => {
 
     it('should recieve exactly one argument', async () => {
         return await expect(getDefinition()).to.eventually.be.rejected;
@@ -15,6 +15,10 @@ describe('Is A Word Test Suite Package', () => {
     
     it('should expect an argument of type string', async () => {
         return await expect(getDefinition(69)).to.eventually.be.rejected;
+    });
+
+    it('should return an object if the word exists', async () => {
+        return await expect(getDefinition('test')).to.eventually.be.an('object');
     });
 
 })
